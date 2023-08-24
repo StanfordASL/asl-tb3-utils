a4501e6016d012904328a712eb4c2e7b874f6e4f
Alvin Sun <alvinsunyixiao@gmail.com>
2023-08-23 19:44:02 -0700

    restructure

--- asl_tb3_sim/launch/root.launch.py
+++ asl_tb3_sim/launch/root.launch.py
@@ -1,22 +1,4 @@
 #!/usr/bin/env python3
-#
-# Copyright 2019 ROBOTIS CO., LTD.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Authors: Joep Tool
-
-import os
 
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
@@ -40,7 +22,7 @@ def generate_launch_description():
 
     gazebo_ros_dir = FindPackageShare("gazebo_ros")
     tb3_gazebo_dir = FindPackageShare("turtlebot3_gazebo")
-    this_dir = FindPackageShare("tb3_sim")
+    this_dir = FindPackageShare("asl_tb3_sim")
 
     gzserver_cmd = IncludeLaunchDescription(
         PathJoinSubstitution([gazebo_ros_dir, "launch", "gzserver.launch.py"]),
