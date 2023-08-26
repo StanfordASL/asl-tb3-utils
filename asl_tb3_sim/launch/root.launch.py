9e123d5aeee6ca7d6f80818601ab3fadb76faaf5
Alvin Sun <alvinsunyixiao@gmail.com>
2023-08-25 21:44:38 -0700

    root launch and SLAM working together

--- asl_tb3_sim/launch/root.launch.py
+++ asl_tb3_sim/launch/root.launch.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import os
+
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
@@ -14,36 +16,40 @@ def generate_launch_description():
 
     world = LaunchConfiguration("world", default=
         PathJoinSubstitution([
-            FindPackageShare("turtlebot3_gazebo"),
+            FindPackageShare("asl_tb3_sim"),
             "worlds",
-            "turtlebot3_world.world",
+            "turtlebot3_world.sdf",
         ])
     )
 
-    gazebo_ros_dir = FindPackageShare("gazebo_ros")
-    tb3_gazebo_dir = FindPackageShare("turtlebot3_gazebo")
     this_dir = FindPackageShare("asl_tb3_sim")
 
-    gzserver_cmd = IncludeLaunchDescription(
-        PathJoinSubstitution([gazebo_ros_dir, "launch", "gzserver.launch.py"]),
-        launch_arguments={"world": world}.items()
+    sdf_file = os.path.join(this_dir.find("asl_tb3_sim"), "models", "asl_tb3.sdf")
+    with open(sdf_file, "r") as f:
+        robot_desc = f.read()
+    robot_state_pub = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[
+            {"use_sim_time": use_sim_time},
+            {"robot_description": robot_desc},
+        ],
     )
 
-    gzclient_cmd = IncludeLaunchDescription(
-        PathJoinSubstitution([gazebo_ros_dir, "launch", "gzclient.launch.py"])
-    )
-
-    robot_state_publisher_cmd = IncludeLaunchDescription(
-        PathJoinSubstitution([tb3_gazebo_dir, "launch", "robot_state_publisher.launch.py"]),
-        launch_arguments={"use_sim_time": use_sim_time}.items()
+    gz_launch = IncludeLaunchDescription(
+        PathJoinSubstitution([
+            FindPackageShare("ros_gz_sim"), "launch", "gz_sim.launch.py",
+        ]),
+        launch_arguments={"gz_args": ["-r ", world]}.items(),
     )
 
     spawn_turtlebot = Node(
-        package="gazebo_ros",
-        executable="spawn_entity.py",
+        package="ros_gz_sim",
+        executable="create",
         arguments=[
-            "-entity", "burger",
-            "-file", PathJoinSubstitution([this_dir, "models", "asl_tb3.sdf"]),
+            "-world", "default",
+            "-file", PathJoinSubstitution([FindPackageShare("asl_tb3_sim"), "models", "asl_tb3.sdf"]),
+            "-name", "asl_tb3",
             "-x", x_init,
             "-y", y_init,
             "-z", "0.01",
@@ -51,12 +57,34 @@ def generate_launch_description():
         output="screen",
     )
 
-    ld = LaunchDescription()
+    bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        parameters=[{
+            "config_file": PathJoinSubstitution([
+                FindPackageShare("asl_tb3_sim"),
+                "configs",
+                "gz_bridge.yaml",
+            ]),
+        }],
+    )
 
-    # Add the commands to the launch description
-    ld.add_action(gzserver_cmd)
-    ld.add_action(gzclient_cmd)
-    ld.add_action(robot_state_publisher_cmd)
-    ld.add_action(spawn_turtlebot)
+    lidar_tf = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        arguments=["0", "0", "0", "0", "0", "0", "1", "base_scan", "asl_tb3/base_scan/velodyne"],
+    )
+    imu_tf = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        arguments=["0", "0", "0", "0", "0", "0", "1", "imu_link", "asl_tb3/imu_link/imu"],
+    )
 
-    return ld
+    return LaunchDescription([
+        robot_state_pub,
+        gz_launch,
+        spawn_turtlebot,
+        bridge,
+        lidar_tf,
+        imu_tf,
+    ])
