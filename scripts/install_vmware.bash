4e82a6d5f67c2d9423ccf7a06454ad44e31fd2af
Alvin Sun <alvinsnuyixiao@gmail.com>
2023-10-01 18:07:06 -0700

    add env var to vm install script

--- scripts/install_vmware.bash
+++ scripts/install_vmware.bash
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 echo "export DISABLE_GUI=1" >> ~/.bashrc
+echo "export INSIDE_VM=1" >> ~/.bashrc
 
 bash <(curl -s https://raw.githubusercontent.com/StanfordASL/asl-tb3-utils/main/scripts/install.bash)
 
