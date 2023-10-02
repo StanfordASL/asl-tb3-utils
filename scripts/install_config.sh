ab09fd07908577b0c9de6e70876db957dea228c3
Alvin Sun <alvinsnuyixiao@gmail.com>
2023-10-01 18:56:17 -0700

    force relink

--- scripts/install_config.sh
+++ scripts/install_config.sh
@@ -4,11 +4,22 @@ echo "Install apt dependencies"
 sudo apt install -qq git python3-dev python3-venv cmake build-essential vim tmux htop gh -y
 
 echo "Symlink dot files"
+if [ -d "$HOME/configs" ]; then
+    mv $HOME/configs $HOME/configs.bak
+    echo "Found existing ~/configs, moved to ~/configs.bak"
+fi
+for f in ".vimrc" ".tmux.conf" ".colcon/defaults.yaml"
+do
+    if [ -f $HOME/$f ]; then
+        mv $HOME/$f $HOME/$f.bak
+        echo "Found existing ~/$f, moved to ~/$f.bak"
+    fi
+done
 mkdir -p ~/.colcon
 git clone https://github.com/alvinsunyixiao/configs.git $HOME/configs
-ln -s $HOME/configs/vim/.vimrc ~
-ln -s $HOME/configs/tmux/.tmux.conf ~
-ln -s $HOME/configs/colcon/defaults.yaml ~/.colcon
+ln -sf $HOME/configs/vim/.vimrc ~
+ln -sf $HOME/configs/tmux/.tmux.conf ~
+ln -sf $HOME/configs/colcon/defaults.yaml ~/.colcon
 
 echo "Configure Vim"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
@@ -16,5 +27,4 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 vim -E +'PlugInstall --sync' +qa
 
 echo "Rebuild workspace"
-update_tb_ws
-
+$HOME/tb_ws/src/asl-tb3-utils/scripts/update.sh
